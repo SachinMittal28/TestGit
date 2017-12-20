aab17042e10c30ad7194008f13e28e8785a4e6cd
SachinMittal28 <34620543+SachinMittal28@users.noreply.github.com>
2017-12-20 18:06:55 +0530

    Update DIGJUMP.java

--- DIGJUMP.java
+++ DIGJUMP.java
@@ -2,51 +2,46 @@ import java.util.Scanner;
 
 public class DIGJUMP {
 
-	void initialiseAdjacencyMatrix(int adjacency_matrix[][], int input) {
+	void initialiseAdjacencyMatrix(int adjacency_matrix[][], String inputStrng) {
 
-		String inputStrng = String.valueOf(input);
 		int length = inputStrng.length();
-		adjacency_matrix = new int[length][length];
-
-		for (int i = 0; i < length; i++)
-			for (int j = 0; j < length; j++)
-				adjacency_matrix[i][j] = 0;
 
 		for (int i = 0; i < length; i++) {
-			for (int j = i ; j < length; j++) {
-				if ((inputStrng.charAt(i) == inputStrng.charAt(j) && i!=j) || j==i+1) {
+			for (int j = i; j < length; j++) {
+				if ((inputStrng.charAt(i) == inputStrng.charAt(j) && i != j) || j == i + 1) {
 					adjacency_matrix[i][j] = 1;
+					adjacency_matrix[j][i] = 1;
 				}
 
 			}
 		}
 
-		for (int i = 0; i < length; i++)
-			for (int j = 0; j < length; j++)
-				if (adjacency_matrix[i][j] == 1 && adjacency_matrix[j][i] == 0)
-					adjacency_matrix[j][i] = 1;
+	}
+
+	void printMatrix(int adjacency_matrix[][]) {
 
 		System.out.println("adjacency matrix is -");
+		for (int i = 0; i < adjacency_matrix.length; i++) {
 
-		for (int i = 0; i < length; i++) {
-
-			for (int j = 0; j < length; j++)
+			for (int j = 0; j < adjacency_matrix[i].length; j++)
 				System.out.print(adjacency_matrix[i][j]);
 
 			System.out.println();
 		}
-
 	}
 
 	public static void main(String[] args) {
-		@SuppressWarnings("resource")
+
 		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();
-		int adjacency_matrix[][] = null;
-
+		String inputStrng = scanner.next();
+		int length = inputStrng.length();
+		int adjacency_matrix[][] =  new int[length][length];
+		
 		DIGJUMP obj = new DIGJUMP();
-		obj.initialiseAdjacencyMatrix(adjacency_matrix, input);
-
+		obj.initialiseAdjacencyMatrix(adjacency_matrix, inputStrng);
+		//obj.printMatrix(adjacency_matrix);
+		
+		
 	}
 
 }
