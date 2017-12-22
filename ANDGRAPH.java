52a6812dfdee64579d330affbac397940f5fdd08
SachinMittal28 <34620543+SachinMittal28@users.noreply.github.com>
2017-12-22 19:09:26 +0530

    Add files via upload

--- ANDGRAPH.java
+++ ANDGRAPH.java
@@ -1,21 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
- class ANDGRAPH {
+class ANDGRAPH {
 	public static void main(String[] args) throws Exception {
 		Scanner scanner = new Scanner(System.in);
 
 		int n = scanner.nextInt();
-		
-		while (n-- > 0) {
-			int num1 = scanner.nextInt();
-			int num2 = scanner.nextInt();
 
-			System.out.println(maxPaths(num1, num2));
+		while (n-- > 0) {
+			String num1 = scanner.next();
+			String num2 = scanner.next();
+			int n1 = Integer.parseInt(num1,2);
+			int n2 = Integer.parseInt(num2,2);
+
+			System.out.println(maxPaths(n1, n2));
 		}
 		scanner.close();
 	}
 
 	private static long maxPaths(int s, int t) {
+		
+		//List<Long> list = new ArrayList<Long>(Collections.nCopies(t-s+1, -1L));
+		List<Long> list = new ArrayList<Long>(t-s+1);
+		for(int i=0;i<list	.size();i++) {
+			list.add(i,-1l);
+		}
+		list.add(0,0l);
 		long out = 0l;
 
 		if (s == t)
@@ -26,8 +38,11 @@ import java.util.Scanner;
 
 		for (int i = s + 1; i <= t; i++) {
 			if ((s & i) == 0) {
-
-				out = out + maxPaths(i, t);
+				
+				if (list.get(t - i) == -1L)
+					list.add(t - i, maxPaths(i, t));
+				
+				out = out + list.get(t - i);
 
 			}
 		}
